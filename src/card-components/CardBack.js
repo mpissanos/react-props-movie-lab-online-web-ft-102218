1557c316d6ad60c9138b49c47d02b43a08aca1ee
Manny Pissanos <mpissano@gmail.com>
2019-04-01 19:08:01 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,7 +11,7 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    
   }
 
   render() {
